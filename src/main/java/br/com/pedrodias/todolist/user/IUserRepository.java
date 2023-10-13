20ead991dc6874f08fcfe1688dbf4a006c14b098
diaspd <pdr050203@gmail.com>
2023-10-12 22:31:43 -0300

    adding validation to app

---
+++ src/main/java/br/com/pedrodias/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.pedrodias.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  UserModel findByUsername(String username);
+}
--- src/main/java/br/com/pedrodias/todolist/user/UserController.java
+++ src/main/java/br/com/pedrodias/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.pedrodias.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      System.out.println("Usuário já existe");
+      return null;
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 }
