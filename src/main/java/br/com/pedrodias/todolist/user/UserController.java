efdc7dec9c411f48f68c4d595ab672c6bd14aef3
diaspd <pdr050203@gmail.com>
2023-10-12 22:19:45 -0300

    added local db

--- src/main/java/br/com/pedrodias/todolist/user/UserController.java
+++ src/main/java/br/com/pedrodias/todolist/user/UserController.java
@@ -11,6 +11,6 @@ public class UserController {
   
   @PostMapping("/")
   public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+    System.out.println(userModel.getUsername());
   }
 }
