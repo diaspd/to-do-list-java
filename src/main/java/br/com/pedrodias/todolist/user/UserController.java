508974f3066021c41dc665a84bf36f602fc41aed
diaspd <pdr050203@gmail.com>
2023-10-12 22:37:18 -0300

    handling errors

--- src/main/java/br/com/pedrodias/todolist/user/UserController.java
+++ src/main/java/br/com/pedrodias/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.pedrodias.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,14 @@ public class UserController {
   private IUserRepository userRepository;
   
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if(user != null) {
-      System.out.println("Usuário já existe");
-      return null;
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
 
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
