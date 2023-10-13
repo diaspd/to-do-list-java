1572735939322b11b3dc812bf64bb07448b17243
diaspd <pdr050203@gmail.com>
2023-10-13 16:25:56 -0300

    adding encryption to the password

--- src/main/java/br/com/pedrodias/todolist/user/UserController.java
+++ src/main/java/br/com/pedrodias/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +24,10 @@ public class UserController {
     if(user != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
