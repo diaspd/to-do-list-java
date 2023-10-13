c5b72f09f18f79f302cf6b673a5a5c4da52c0876
diaspd <pdr050203@gmail.com>
2023-10-13 17:40:21 -0300

    added tasks

---
+++ src/main/java/br/com/pedrodias/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.pedrodias.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+  
+}
