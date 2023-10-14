d28baded5e992c5b260eabc42eb49f4e46db7476
diaspd <pdr050203@gmail.com>
2023-10-14 10:31:06 -0300

    adding edition

--- src/main/java/br/com/pedrodias/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/pedrodias/todolist/task/ITaskRepository.java
@@ -1,9 +1,10 @@
 package br.com.pedrodias.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-  
+  List<TaskModel> findByIdUser(UUID idUser);
 }
--- src/main/java/br/com/pedrodias/todolist/task/TaskController.java
+++ src/main/java/br/com/pedrodias/todolist/task/TaskController.java
@@ -1,6 +1,13 @@
 package br.com.pedrodias.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +23,27 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-    System.out.println("Chegou no controller" + request.getAttribute("idUser"));
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término deve ser maior que a data atual");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   }
-  
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
+  }
 }
